d2106f02e0662a2ff352ae6e8504d87836b00166
Ibai Fernández <info@ibaifernandez.com>
2023-01-05 16:34:15 -0500

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import "../style/index.css";
 
-/**
+/*
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
+ *  This function is called every time the user types within an input or changes one of them
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should
@@ -10,7 +10,7 @@ import "../style/index.css";
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "left", // social media bar position (left or right)
         
-        twitter: null, // social media usernames
+        twitter: null,
         github: null,
         linkedin: null,
         instagram: null,
@@ -22,25 +22,32 @@ import "../style/index.css";
         city: null
     }
  */
+
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
+  console.log("These are the current variables: ", variables);
+
   // here we ask the logical questions to make decisions on how to build the html
+
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.includeCover == false) {
+    cover = "<div class='cover'></div>";
+  } else {
+    alert("We should be displaying where to get an image from.");
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter} target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github} target="_blank"><i class="fa-brands fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin} target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram} target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -52,11 +59,12 @@ function render(variables = {}) {
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should
-    includeCover: true,
+    includeCover: false,
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://thumbs.dreamstime.com/b/default-avatar-profile-flat-icon-social-media-user-vector-portrait-unknown-human-image-default-avatar-profile-flat-icon-184330869.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
@@ -64,11 +72,11 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
   render(window.variables); //render the card for the first time
 
